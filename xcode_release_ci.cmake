518873b9edbee6a84dfa2565c35b13d3cc625363
Thomas Jandecka <thomas.jandecka@oracle.com>
2018-11-05 18:33:43 +0100

    ci: experiment

---
+++ xcode_release_ci.cmake
@@ -0,0 +1,14 @@
+cmake_minimum_required(VERSION 3.5)
+set(CTEST_PROJECT_NAME "Drafter")
+
+set(CTEST_SOURCE_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}")
+set(CTEST_BINARY_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}/build")
+
+set(CTEST_USE_LAUNCHERS 1)
+
+ctest_start("Continuous")
+ctest_configure(OPTIONS "-DCMAKE_BUILD_TYPE=Release")
+ctest_build()
+ctest_test()
+ctest_memcheck()
+ctest_submit()
